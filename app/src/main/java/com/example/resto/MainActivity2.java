cc054bf0b4142ba064572c30596e0d5f2d51d8c5
sunnysamuelms <sunnysamuel2112@gmail.com>
2022-06-02 23:48:52 +0530

    fixed theme error

--- app/src/main/java/com/example/resto/MainActivity2.java
+++ app/src/main/java/com/example/resto/MainActivity2.java
@@ -2,13 +2,31 @@ package com.example.resto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn_logout = findViewById(R.id.buttonLogout);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 }

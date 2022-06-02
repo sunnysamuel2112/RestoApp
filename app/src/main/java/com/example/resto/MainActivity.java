429de1fd4b2cf801347103169c2b33ce7341e88d
sunnysamuelms <sunnysamuel2112@gmail.com>
2022-06-03 01:01:10 +0530

    removed unwanted imports

--- app/src/main/java/com/example/resto/MainActivity.java
+++ app/src/main/java/com/example/resto/MainActivity.java
@@ -1,21 +1,20 @@
 package com.example.resto;
 
 //import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 // this is to check github branch
 public class MainActivity extends AppCompatActivity {
 
     private Button editLoginButton;
-    private TextView editTextForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

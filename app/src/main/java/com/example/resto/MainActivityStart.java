9d459bc06632eb0a35412e7a9154dbf332946858
sunnysamuelms <sunnysamuel2112@gmail.com>
2022-07-06 00:16:56 +0530

    added recycler view and drawable imgs

---
+++ app/src/main/java/com/example/resto/MainActivityStart.java
@@ -0,0 +1,14 @@
+package com.example.resto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_start);
+    }
+}
